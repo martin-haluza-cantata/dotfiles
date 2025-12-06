5e627ea372fe5d29a84d4769ab60e5f0b5beeef8
Martin Haluza <100014144+martin-haluza-cantata@users.noreply.github.com>
2025-12-06 18:54:57 +0100

    updated to work with gpg

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# Create .ssh directory if it doesn't exist
+mkdir -p ~/.ssh
+ssh-add -L > ~/.ssh/signing_key.pub
+# Copy SSH public key for Git signing
+# cp id_ed25519_sk_git_signing.pub ~/.ssh/
+
 # Copy Git configuration
 cp .gitconfig ~/.gitconfig
 
-# Create .ssh directory if it doesn't exist
-mkdir -p ~/.ssh
 
-# Copy SSH public key for Git signing
-cp id_ed25519_sk_git_signing.pub ~/.ssh/
 
 echo "Dotfiles installed successfully!"
